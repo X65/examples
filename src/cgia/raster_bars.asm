7db42d63575ec9cf4de85ce3f94da933245d065b
Tomasz Sterna <tomasz@sterna.link>
2025-08-15 22:47:48 +0200

    Start raster_bars from the top of screen

--- src/cgia/raster_bars.asm
+++ src/cgia/raster_bars.asm
@@ -14,31 +14,30 @@
 reset:
     sei     ; we do not use interrupts in this demo
 
-    lda #0              ; start with black color
-    ldx #FRAME_DELAY    ; init delay loop
+    ldx #FRAME_DELAY            ; init delay loop
+    lda #0                      ; start with black color
 
 forever:
-    sta CGIA::back_color ; set background color
-    ldy #1              ; wait for next line
+    ldy #0                      ; wait for line 0 (actually starts in front porch)
+:   cpy CGIA::raster            ; check if we are on 0 raster line
+    bne :-                      ; loop if not
+    sta CGIA::back_color        ; set background color
+    iny                         ; wait for next line
 
 wait:
-    cpy CGIA::raster    ; check if we are on Y raster line
-    bne wait            ; loop if not
+    cpy CGIA::raster            ; check if we are on Y raster line
+    bne wait                    ; loop if not
 
-    inc CGIA::back_color ; increase background color
+    inc CGIA::back_color        ; increase background color
 
-    iny                 ; move to next raster line
-    cpy #240            ; check if we reached line 240
-    bcc wait            ; wait for next line if not
+    iny                         ; move to next raster line
+    cpy #240                    ; check if we reached line 240
+    bcc wait                    ; wait for next line if not
 
-    ldy #0              ; will wait for line 0 again
-
-    dex                 ; delay moving the bars
+    dex                         ; delay moving the bars
     bne forever
-    ldx #FRAME_DELAY    ; reload delay
+    ldx #FRAME_DELAY            ; reload delay
 
-    ; inc                 ; start with next color
-    clc
-    adc #1
+    inc A                       ; start with next color
 
     jmp forever
